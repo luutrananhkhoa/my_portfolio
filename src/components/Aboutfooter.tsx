778a2e3b5b5833ce3096b87422f925e0323b75dd
Lưu Trần Anh Khoa <luutrananhkhoa@Mac-mini-cua-Luu.local>
2025-05-16 20:33:05 +0700

    ~style: update style for skill logo

--- src/components/Aboutfooter.tsx
+++ src/components/Aboutfooter.tsx
@@ -11,9 +11,9 @@ const Aboutfooter = () => {
         {
           items.map((val, indx) => {
               return(
-                <div className="p-1 w-fit relative" key={indx}>
-                  <h1 className="gap-2 text-xl font-poppins text-primary-text font-semibold relative flex icon_underline max-sm:text-2xl">{val.icon}{val.name}</h1>
-                  <div className="flex gap-2 justify-center items-center flex-row text-2xl text-primary-text pt-3 max-lg:justify-start">
+                <div className="p-1 w-fit relative mt-4" key={indx}>
+                  <h1 className="gap-2 text-xl font-poppins text-primary-text font-bold relative flex icon_underline max-sm:text-2xl">{val.icon}{val.name}</h1>
+                  <div className="flex gap-2 justify-center items-center flex-row text-xl text-primary-text pt-3 max-lg:justify-start">
                     {val.answer}
                   </div>
               </div>
--- src/components/SkillsFotter.tsx
+++ src/components/SkillsFotter.tsx
@@ -12,7 +12,7 @@ const SkillsFooter:React.FC<MyComponentProps> = ({items}) => {
       { items && items.map((val, indx) => {
         return (
           <div className="p-4" key={indx}>
-            <Image src={val?.img} alt={val?.alt} className="w-auto max-h-10" />
+            <Image src={val?.img} alt={val?.alt} className="w-auto max-h-10 max-w-10" />
           </div>
         );
       })}
